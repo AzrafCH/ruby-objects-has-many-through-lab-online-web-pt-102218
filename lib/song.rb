adf77fa8e39805fc139cd1b15c639fe2f5e8bca7
Azraf Chowdhury <achowdhur@ltu.edu>
2018-11-26 06:40:09 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,18 +1,15 @@
-class Song 
-  
- attr attr_accessor :name, :artist, :genre 
-  
- @@all = []
-  
- def initialize(name,artist,genre)
-  @name = name
-  @artist = artist 
-  @genre = genre 
-  @@all << self 
-  @genre.songs << self
- end 
- 
- def self.all
-   @@all 
- end 
-end 
+
+class Song
+  attr_accessor :name, :artist, :genre
+  @@all = []
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@all << self
+    @genre.songs << self
+  end
+   def self.all
+    @@all
+  end
+end
